42240cf5aab551c49e93ffe9372b136a3d05f063
yublee71 <lybin71@naver.com>
2024-04-10 16:35:32 +0100

    working well when specified with /usr/bin/

--- src/main.c
+++ src/main.c
@@ -3,14 +3,13 @@
 int	main(int argc, char *argv[])
 {
 	int		fd[2];
-	// int		fd_file;
-	int		i;
-	char	*args[argc];
+	int		fd_output;
+	char	*args[4];
 	pid_t	pid;
-	char	buf[BUFFERSIZE] = {0};
+	// char	buf[BUFFERSIZE] = {0};
 	
-	if (argc < 2 && argv)
-		return 1;
+	if (argc < 2)
+		exit (1);
 
 	if (pipe(fd) < 0)
 	{
@@ -29,27 +28,38 @@ int	main(int argc, char *argv[])
 	else if (pid == 0)
 	{
 		close(fd[0]);
-		// fd_file = open("./output.txt", O_RDWR | O_TRUNC);
 		dup2(fd[1], 1);
 
-		i = -1;
-		while (++i < argc - 1)
-			args[i] = argv[i + 1];
-		args[i] = NULL;
+		args[0] = argv[2];
+		args[1] = argv[3];
+		args[2] = argv[1];
+		args[3] = NULL;
 
 		if (execve(args[0], args, NULL) == -1)
 		{
 			perror("execve");
-			return 1;
+			exit (1);
 		}
 	}
 
 	else
 	{
 		close(fd[1]);
-		read(fd[0], buf, BUFFERSIZE);
-		printf("%s\n", buf);
-	}
+		dup2(fd[0], 0);
+		fd_output = open(argv[6], O_WRONLY | O_TRUNC);
+		dup2(fd_output, 1);
 
-	return 0;
+		args[0] = argv[4];
+		args[1] = argv[5];
+		args[2] = NULL;
+		args[3] = NULL;
+
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("execve");
+			exit (1);
+		}
+		// read(0, buf, BUFFERSIZE);
+		// printf("%s\n", buf);
+	}
 }
