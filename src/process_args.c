49861438a323216bb64272938bc7e76f2697a520
Yubeen Lee <lybin71@naver.com>
2024-04-15 22:55:07 +0900

    better with memory management, test:OK

--- src/process_args.c
+++ src/process_args.c
@@ -1,5 +1,17 @@
 #include "pipex.h"
 
+//./pipex infile "ls -l" "wc -l" outfile
+
+void	free_str_array(char* str_array[])
+{
+	int	i;
+
+	i = 0;
+	while (str_array[i])
+		free(str_array[i++]);
+	free(str_array);
+}
+
 char **get_args(char *argv)
 {
 	char	**args;
@@ -7,21 +19,29 @@ char **get_args(char *argv)
 	int		flag;
 	char	*path;
 	char	**paths;
+	char	*temp;
 
 	flag = 0;
 	i = 0;
-	args = ft_split(argv, ' ');
+	args = ft_split(argv, ' '); //needs free
 	while (!ft_strnstr(environ[i], "PATH", 4))
 		i++;
 	path = ft_strnstr(environ[i], "PATH", 4) + 5;
-	paths = ft_split(path, ':');
+	paths = ft_split(path, ':'); //needs free
 	i = 0;
 	while (paths[i])
 	{
+		temp = paths[i];
 		paths[i] = ft_strjoin(paths[i], "/");
-		if (!access(ft_strjoin(paths[i], args[0]), X_OK))
+		free(temp);
+		temp = paths[i];
+		paths[i] = ft_strjoin(paths[i], args[0]);
+		free(temp);
+		if (!access(paths[i], X_OK))
 		{
-			args[0] = ft_strjoin(paths[i], args[0]);
+			temp = args[0];
+			args[0] = paths[i];
+			free(temp);
 			flag = 1;
 			break;
 		}
