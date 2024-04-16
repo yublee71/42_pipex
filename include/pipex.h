3566e3b7dd9bd8079840099e83b6e0a40bff5601
yublee71 <lybin71@naver.com>
2024-04-16 13:53:30 +0100

    header file modified

--- include/pipex.h
+++ include/pipex.h
@@ -6,16 +6,18 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 13:55:33 by yublee            #+#    #+#             */
-/*   Updated: 2024/04/16 00:38:50 by yublee           ###   ########.fr       */
+/*   Updated: 2024/04/16 13:53:07 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include "libft.h"
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include "libft.h"
 
 extern char	**environ;
 
@@ -26,3 +28,5 @@ void	join_path(char **paths, char **args);
 void	check_path(char **paths, char **args);
 void	exit_with_error(const char *str, int errno);
 void	free_str_array(char **array);
+
+#endif
