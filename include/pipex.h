5a904b0b0121b52e7a2d88ef64288ef277a044b8
yublee71 <lybin71@naver.com>
2024-04-23 16:50:58 +0100

    error handling fixed, bonus added

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 13:55:33 by yublee            #+#    #+#             */
-/*   Updated: 2024/04/16 13:53:07 by yublee           ###   ########.fr       */
+/*   Updated: 2024/04/23 16:39:07 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <fcntl.h>
 # include <stdio.h>
 # include <unistd.h>
+# include <errno.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 # include "libft.h"
@@ -26,7 +27,7 @@ void	parent_process(int fd[2], char *argv[]);
 char	**get_args(char *argv);
 void	join_path(char **paths, char **args);
 void	check_path(char **paths, char **args);
-void	exit_with_error(const char *str, int errno);
+void	exit_with_error(const char *str, int exit_no);
 void	free_str_array(char **array);
 
 #endif
