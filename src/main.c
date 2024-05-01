b854e909a7597e73a8429ad7263319dd2d7a621d
Yubeen Lee <lybin71@naver.com>
2024-05-01 21:19:22 +0100

    first

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 00:37:53 by yublee            #+#    #+#             */
-/*   Updated: 2024/04/16 00:38:22 by yublee           ###   ########.fr       */
+/*   Updated: 2024/05/01 21:11:07 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,26 @@
 int	main(int argc, char *argv[])
 {
 	int		fd[2];
+	int		cmd_cnt;
+	int		i;
 	pid_t	pid;
 
-	if (argc < 5)
+	cmd_cnt = argc - 3;
+	i = 0;
+	if (cmd_cnt < cmd_cnt)
 		exit_with_error("bad arguments", EXIT_FAILURE);
 	if (pipe(fd) < 0)
 		exit_with_error("pipe error", EXIT_FAILURE);
-	pid = fork();
-	if (pid < 0)
-		exit_with_error("fork error", EXIT_FAILURE);
-	else if (pid == 0)
-		child_process(fd, argv);
-	else
-		parent_process(fd, argv);
+	while (i < cmd_cnt)
+	{
+		pid = fork();
+		if (pid < 0)
+			exit_with_error("fork error", EXIT_FAILURE);
+		else if (pid == 0)
+			child_process(fd, argv);
+		i++;
+	}
+	while (wait(NULL) != -1)
+		;
 	exit (EXIT_SUCCESS);
 }
